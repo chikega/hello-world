41fc3f06971e6666ba8c537dd27d857865e7e496
Gary Chike <chikega@pm.me>
2022-04-26 14:06:44 -0400

    Testing syntax highlighting

---
+++ sortString.c
@@ -0,0 +1,22 @@
+/* Gary Chike                                 03/06/2022 */
+
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+    char word1[50];
+    char word2[50];
+
+    printf("Please enter a word: ");
+    scanf("%s", word1);
+    printf("And another: ");
+    scanf("%s", word2);
+    // Firnd first letter in which words differ
+    while (word1[i]!='\0' && word2[i]!= '\0' && word1[i] == word2[i])
+    i++;
+    if (word1[i] < word2[i])
+        printf("%s come before %s in the alphabet.\n", word1, word2);
+    else if (word1[i]> word2[i])
+        printf("%s come before %s in the alphabet.\n", word1, word2);
+    else printf("You entered the same word, \"%s\", twice.\n", word1);
+}
